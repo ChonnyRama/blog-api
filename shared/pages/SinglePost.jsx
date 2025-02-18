63527b2a5cfcbe75e9b4808b09c645e102eeead1
chonny <hitsugiya@gmail.com>
2025-02-18 16:06:42 -0500

    add single pages for each post

--- shared/pages/SinglePost.jsx
+++ shared/pages/SinglePost.jsx
@@ -1,7 +1,32 @@
+import {format} from 'date-fns'
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import styled from 'styled-components'
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  margin: 0 auto;  // Center the container within its parent
+  align-items: start;
+  justify-content: center;
+  text-align: left;
+`
 
 export default function SinglePost() {
+  const location = useLocation()
+  const post = location.state?.post
+  const formattedDate = format(new Date(post.createdAt), 'MMM dd yyyy')
+
+
+
+
+
   return (
-    <div>SinglePost</div>
+    <PostContainer>
+      <h2>{post.title}</h2>
+      <span>Chonma | {formattedDate}</span>
+      <p>{post.content}</p>
+    </PostContainer>
   )
 }
